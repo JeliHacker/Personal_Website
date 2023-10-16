34260f22556966b68368d0f44ba177df42b6909d
Eli Gooch <jeligooch@gmail.com>
2023-10-15 21:22:35 -0700

    add quizbowl.io build

---
+++ blog/precache-manifest.5854c61684f10d51deb96b7fbcf059e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c30153873083ead956",
+    "url": "/blog/static/css/main.7b8e95ad.chunk.css"
+  },
+  {
+    "revision": "30c30153873083ead956",
+    "url": "/blog/static/js/main.756be5d1.chunk.js"
+  },
+  {
+    "revision": "85c1996b77f03eea3801",
+    "url": "/blog/static/js/runtime~main.63e52667.js"
+  },
+  {
+    "revision": "4e0e6436144d480f1e14",
+    "url": "/blog/static/js/2.18a78129.chunk.js"
+  },
+  {
+    "revision": "9bf6d873c0fbcceb4668",
+    "url": "/blog/static/js/3.15c248bd.chunk.js"
+  },
+  {
+    "revision": "ddcc33f09efcfee6c1b0f2c5853179ac",
+    "url": "/blog/index.html"
+  }
+];
